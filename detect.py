f562a5833aa36af030ed7974c580c65926ca94f3
Rômulo Maciel <rmul.maciel@gmail.com>
2025-11-18 16:24:21 -0300

    :recycle: remove venv files and refactor detect code

--- detect.py
+++ detect.py
@@ -13,7 +13,7 @@ camera_matrix = np.array([
 dist_coeffs = np.zeros((5, 1), dtype=np.float32)
 marker_length = 0.05
 
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_5X5_100)
 parameters = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, parameters)  # Novo objeto detector
 
@@ -27,17 +27,19 @@ while True:
     corners, ids, rejected = detector.detectMarkers(gray)
 
     if ids is not None:
-        rvecs, tvecs, _ = detector.estimatePoseSingleMarkers(
-            corners, marker_length, camera_matrix, dist_coeffs
-        )
+        # rvecs, tvecs, _ = detector.estimatePoseSingleMarkers(
+        #     corners, marker_length, camera_matrix, dist_coeffs
+        # )
 
         for i in range(len(ids)):
-            cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], marker_length)
+            # cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], marker_length)
             
-            x, y, z = tvecs[i][0]
-            cv2.putText(frame, f"X:{x:.2f} Y:{y:.2f} Z:{z:.2f}", (10, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            # x, y, z = tvecs[i][0]
+            # cv2.putText(frame, f"X:{x:.2f} Y:{y:.2f} Z:{z:.2f}", (10, 30),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
             cv2.putText(frame, f"ID: {ids[i][0]}", (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            cv2.putText(frame, f"Pixel pos: ({int(corners[i][0][0][0])}, {int(corners[i][0][0][1])})", (10, 90),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
             aruco.drawDetectedMarkers(frame, corners, ids)
 
